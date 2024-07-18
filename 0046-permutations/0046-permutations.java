3622edcff59429b78ce69d4d57d297264318252c
이보윤 <qhdbs0802@naver.com>
2024-07-18 17:41:23 +0900

    Time: 9 ms (5.09%), Space: 44.9 MB (8.7%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -5,12 +5,15 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
         backtrack(new ArrayList<>(), nums, visited, result);
+        for (List<Integer> permutation : result) {
+            System.out.println(permutation);
+        }
         return result;
     }
 
     // 순열을 만들어서 result에 추가하는 함수
     void backtrack(List<Integer> curr, int[] nums, boolean[] visited, List<List<Integer>> result) {
-        // basecase
+        // base case
         if (curr.size() == nums.length) {
             result.add(new ArrayList<>(curr));
             return;
@@ -20,21 +23,11 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (visited[i]) continue;
 
-            curr.add(nums[i]); // add
+            curr.add(nums[i]);
             visited[i] = true;
             backtrack(curr, nums, visited, result);
             curr.remove(curr.size() - 1); // remove 마지막 원소
             visited[i] = false;
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {1, 2, 30, 40};
-        List<List<Integer>> permutations = solution.permute(nums);
-
-        for (List<Integer> permutation : permutations) {
-            System.out.println(permutation);
         }
     }
 }
